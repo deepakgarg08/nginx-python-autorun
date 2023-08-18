4f9f0f24838d44626d3c2035bbe841f946077dcc
Deepak Garg <deepak.garg@dfki.de>
2023-08-18 21:53:04 +0200

    Add intentional error route

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # return 'Web App with Python Flask!'
-    return render_template('index.html')
+    return render_template('index.html')  # Renders the index.html template
+
+@app.route('/error')
+def error():
+    # Simulate an error by raising an exception
+    raise Exception("An intentional error occurred.")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81)
